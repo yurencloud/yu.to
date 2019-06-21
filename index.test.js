4afb6702c8541e2d12cb625644accbed55580c67
“mackwang” <641212003@qq.com>
2019-06-21 14:10:42 +0800

    已经完成基本功能，准备增加子属性的数组支持

--- index.test.js
+++ index.test.js
@@ -1,4 +1,93 @@
 var to = require('./src/index')
+var data = require('./test-data')
+
+test('字符串型单个配置(最简单的使用)', () => {
+    var obj = {
+        status: 1,  // 0 不健康 1 健康
+    }
+    to(obj, 'status:string')
+
+    expect(obj.status).toBe('1')
+})
+
+test('字符串型配置(以&符号分隔)', () => {
+    var obj = {
+        status: 1,  // 0 不健康 1 健康
+    }
+    to(obj, 'status:rename:statusLabel&statusLabel:enum:不健康,健康')
+
+    expect(obj.statusLabel).toBe('健康')
+})
+
+// 没有值的，一律不支持对象型配置，比如string,number,boolean
+
+test('对象型单个配置(值为字符串)', () => {
+    var obj = {
+        status: 1,  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: 'enum:不健康,健康'
+    })
+
+    expect(obj.status).toBe('健康')
+})
+
+test('对象型多个配置(值为字符串)', () => {
+    var obj = {
+        status: 1,  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: 'rename:statusLabel',
+        statusLabel: 'enum:不健康,健康'
+    })
+    expect(obj.statusLabel).toBe('健康')
+})
+
+test('对象型单个配置(值为对象,值的值为字符串)', () => {
+    var obj = {
+        status: 1,  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: {enum: '不健康,健康'}
+    })
+
+    expect(obj.status).toBe('健康')
+})
+
+test('对象型多个配置(值为对象,值的值为字符串)', () => {
+    var obj = {
+        status: 1,  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: {rename: 'statusLabel'},
+        statusLabel: {enum: '不健康,健康'}
+    })
+    expect(obj.statusLabel).toBe('健康')
+})
+
+
+test('对象型单个配置(值为对象,值的值为对象)', () => {
+    var obj = {
+        status: 'health',  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: {mapping: {unHealth: '不健康', health: '健康'}},
+    })
+
+    expect(obj.status).toBe('健康')
+})
+
+test('对象型多个配置(值为对象,值的值为对象)', () => {
+    var obj = {
+        status: 'health',  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: {rename: 'statusLabel'},
+        statusLabel: {mapping: {unHealth: '不健康', health: '健康'}},
+    })
+
+    expect(obj.statusLabel).toBe('健康')
+})
 
 test('重命名属性', () => {
     var obj = {
@@ -12,19 +101,37 @@ test('重命名属性', () => {
     expect(obj.myname).toBe('mack')
 })
 
+test('重命名属性2', () => {
+    var obj = {
+        name: 'mack',
+    }
+    to(obj, {
+        name: {rename: 'myname'},
+    })
+
+    expect(obj.name).toBe(undefined)
+    expect(obj.myname).toBe('mack')
+})
+
 test('类型转换', () => {
     var obj = {
         age: 23,
+        money: '123.23',
+        love: 'yes',
     }
 
     to(obj, {
         age: 'string',
+        money: 'number',
+        love: 'boolean'
     })
 
     expect(obj.age).toBe('23')
+    expect(obj.money).toBe(123.23)
+    expect(obj.love).toBe(true)
 })
 
-test('日期转换', () => {
+test('时间戳转日期', () => {
     var obj = {
         birthday: 1560827789638,
     }
@@ -35,12 +142,122 @@ test('日期转换', () => {
     expect(obj.birthday).toBe('2019-06-18 11:16:29')
 })
 
+test('时间戳转日期2', () => {
+    var obj = {
+        birthday: 1560827789638,
+    }
+    to(obj, {
+        birthday: {date: ''},
+    })
+
+    expect(obj.birthday).toBe('2019-06-18 11:16:29')
+})
+
+test('时间戳转日期3', () => {
+    var obj = {
+        birthday: 1560827789638,
+    }
+    to(obj, {
+        birthday: {date: 'yyyy-MM-dd'},
+    })
+
+    expect(obj.birthday).toBe('2019-06-18')
+})
+
+test('日期转时间戳', () => {
+    var obj = {
+        birthday: '2019-06-18 11:16:29',
+    }
+    to(obj, {
+        birthday: 'timestamp',
+    })
+
+    expect(obj.birthday).toBe(1560827789000)
+})
+
+test('日期转时间戳2', () => {
+    var obj = {
+        birthday: '2019-06-18 11:16:29',
+    }
+    to(obj, {
+        birthday: {timestamp: ''},
+    })
+
+    expect(obj.birthday).toBe(1560827789000)
+})
+
+test('日期转时间戳3', () => {
+    var obj = {
+        birthday: '2019-06-18',
+    }
+    to(obj, {
+        birthday: {timestamp: 'yyyy-MM-dd'},
+    })
+
+    expect(obj.birthday).toBe(1560787200000)
+})
+
 test('枚举转换', () => {
     var obj = {
         status: 1,  // 0 不健康 1 健康
     }
     to(obj, {
         status: 'enum:不健康,健康',
+    })
+
+    expect(obj.status).toBe('健康')
+})
+
+test('枚举转换2', () => {
+    var obj = {
+        status: 1,  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: {enum: '不健康,健康'},
+    })
+
+    expect(obj.status).toBe('健康')
+})
+
+test('枚举转换3', () => {
+    var obj = {
+        status: 1,  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: {enum: ['不健康', '健康']},
+    })
+
+    expect(obj.status).toBe('健康')
+})
+
+test('枚举对象转换', () => {
+    var obj = {
+        status: 'health',  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: 'mapping:unHealth:不健康,health:健康',
+    })
+
+    expect(obj.status).toBe('健康')
+})
+
+test('枚举对象转换2', () => {
+    var obj = {
+        status: 'health',  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: {mapping: 'unHealth:不健康,health:健康'},
+    })
+
+    expect(obj.status).toBe('健康')
+})
+
+test('枚举对象转换3', () => {
+    var obj = {
+        status: 'health',  // 0 不健康 1 健康
+    }
+    to(obj, {
+        status: {mapping: {unHealth: '不健康', health: '健康'}},
     })
 
     expect(obj.status).toBe('健康')
@@ -94,6 +311,21 @@ test('无限深度属性处理', () => {
     expect(obj.father.child.myname).toBe('tom')
 })
 
+test('无限深度属性处理2', () => {
+    var obj = {
+        father: {
+            child: {
+                name: 'tom'
+            }
+        }
+    }
+    to(obj, {
+        'father.child.name': {rename: 'myname'},
+    })
+    expect(obj.father.child.name).toBe(undefined)
+    expect(obj.father.child.myname).toBe('tom')
+})
+
 test('字符串转数组', () => {
     var obj = {
         words: 'a,b,c',
@@ -105,6 +337,19 @@ test('字符串转数组', () => {
     })
     expect(obj.words[2]).toBe('c')
     expect(obj.words2[2]).toBe('c')
+})
+
+test('default转默认值', () => {
+    var obj = {
+        words: null,
+        words2: null,
+    }
+    to(obj, {
+        words: 'default',
+        words2: 'default:-',
+    })
+    expect(obj.words).toBe('/')
+    expect(obj.words2).toBe('-')
 })
 
 test('处理数组对象', () => {
@@ -130,14 +375,29 @@ test('处理数组对象', () => {
     expect(objArray[1].status).toBe('不健康')
 })
 
-test('字符串参数', () => {
-    var obj = {
-        status: 1,  // 0 不健康 1 健康
-    }
-    to(obj, 'status:rename:statusLabel&statusLabel:enum:不健康,健康')
+test('处理数组对象2', () => {
+    var objArray = [
+        {
+            name: 'mack',
+            age: 23,
+            birthday: 1560827789638,
+            status: 1,  // 0 不健康 1 健康
+        },
+        {
+            name: 'cindy',
+            age: 21,
+            birthday: 1560827789638,
+            status: 0,  // 0 不健康 1 健康
+        },
+    ]
+    to(objArray, {
+        status: {enum: ['不健康', '健康']},
+    })
 
-    expect(obj.statusLabel).toBe('健康')
+    expect(objArray[0].status).toBe('健康')
+    expect(objArray[1].status).toBe('不健康')
 })
+
 
 test('参数数组', () => {
     var obj = {
@@ -148,4 +408,90 @@ test('参数数组', () => {
         }
     )
     expect(obj.status).toBe('健康')
+})
+
+
+test('参数数组2', () => {
+    var obj = {
+        status: 1,  // 0 不健康 1 健康
+    }
+    to(obj, {
+            status: [{copy: 'statusLabel'}, {enum: ['不健康', '健康']}]
+        }
+    )
+    expect(obj.status).toBe('健康')
+})
+
+test('拓展功能', () => {
+    to.extend({
+        addOne: function (source, key, configValue) {
+            source[key] = Number(source[key]) + 1
+        }
+    })
+    var obj = {
+        status: '1',
+    }
+    to(obj, {
+            status: 'addOne'
+        }
+    )
+    expect(obj.status).toBe(2)
+})
+
+test('拓展功能2', () => {
+    to.extend({
+        addOne: function (source, key, configValue) {
+            source[key] = Number(source[key]) + 1
+        }
+    })
+    var obj = {
+        status: '1',
+    }
+    to(obj, {
+            status: {addOne: ''}
+        }
+    )
+    expect(obj.status).toBe(2)
+})
+
+test('拓展功能3', () => {
+    to.extend({
+        addOne: function (source, key, configValue) {
+            source[key] = Number(source[key]) + 1
+        }
+    })
+    var obj = {
+        status: '1',
+    }
+    to(obj, {
+            status: {addOne: ''}
+        }
+    )
+
+    to.extend({
+        addTwo: function (source, key, configValue) {
+            source[key] = Number(source[key]) + 2
+        }
+    })
+
+    to(obj, {
+            status: [{addOne: ''},{addTwo: ''}]
+        }
+    )
+    expect(obj.status).toBe(5)
+})
+
+test('处理分页数据', () => {
+    var dataSource = Object.assign({}, data)
+    to(dataSource.data, {
+        status: 'enum:交易成功,交易失败,正在交易',
+        dispatchTime: 'date',
+        createTime: 'date',
+        updateTime: 'date',
+        remarks: 'default',
+    })
+
+    console.log(dataSource)
+    // expect(objArray[0].status).toBe('健康')
+    // expect(objArray[1].status).toBe('不健康')
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 'use strict'
 
 var date = require('yu.date');
+var object = require('yu.object');
+
+var globalConfig = {
+    default: '/',
+    dateFormat: 'yyyy-MM-dd hh:mm:ss'
+}
 
 var globalExtend = {}
 
@@ -14,88 +20,136 @@ function getOptionFromString(option) {
     var result = {}
     for (var i = 0; i < optionList.length; i++) {
         var itemList = optionList[i].split(':')
-        result[itemList[0]] = optionList[i].substr(itemList[0].length+1)
+        result[itemList[0]] = optionList[i].substr(itemList[0].length + 1)
     }
     return result
 }
 
 /*
-* 将单个字符串配置，转成配置对象
-* @param {String} optionString - 单个字符串配置
-* @return {Object} option - 配置对象 {option:'配置名',value:'配置值'}
+* 将单个字符串配置或配置对象，转成配置对象
+* @param {String, Object} optionItem - 单个字符串配置或配置对象
+* @return {Object} config - 配置对象 {option:'配置名',value:'配置值'}
 * */
-function getOptionConfig(optionString) {
-    var option = optionString.split(':')
+function getOptionConfig(optionItem) {
+    if (typeof optionItem === 'object') {
+        for (var key in optionItem) {//用javascript的for/in循环遍历对象的属性
+            return {option: key, value: optionItem[key]}
+        }
+    }
+    var option = optionItem.split(':')
     if (option.length === 1) {
         return {option: option[0].trim()}
     } else {
         // 修复value中可能出现冒号的问题
-        var value = optionString.substr(option[0].length+1)
+        var value = optionItem.substr(option[0].length + 1)
         return {option: option[0].trim(), value: value}
     }
 }
 
 /*
 * 对操作数据对象应用单个配置对象
-* @param {Object} sourceObject - 操作数据对象
-* @param {String} optionValue - 字符串参数
+* @param {Object} source - 操作数据对象
+* @param {String} optionItem - 字符串参数
 * @return {String} key - 操作数据对象的属性名
 * */
-function singleConversion(sourceObject, optionValue, key) {
-    var config = getOptionConfig(optionValue)
-    if(key.indexOf('.')>0){
+function singleConversion(source, optionItem, key) {
+    var config = getOptionConfig(optionItem)
+    if (key.indexOf('.') > 0) {
         var keys = key.split('.')
         for (var i = 0; i < keys.length - 1; i++) {
-            sourceObject = sourceObject[keys[i]]
-            key = keys[i+1]
+            source = source[keys[i]]
+            key = keys[i + 1]
         }
     }
     switch (config.option) {
         case 'copy':
-            sourceObject[config.value] = sourceObject[key]
+            source[config.value] = source[key]
             break
         case 'rename':
-            sourceObject[config.value] = sourceObject[key]
-            delete sourceObject[key]
+            source[config.value] = source[key]
+            delete source[key]
+            break
+        case 'default':
+            var placeholder = globalConfig.default
+            if (config.value && config.value.length) {
+                placeholder = config.value
+            }
+            source[key] = source[key] === null ? placeholder : source[key]
             break
         case 'string':
-            sourceObject[key] = sourceObject[key] + ''
+            source[key] = String(source[key])
+            break
+        case 'number':
+            source[key] = Number(source[key])
+            break
+        case 'boolean':
+            source[key] = Boolean(source[key])
             break
         case 'date':
-            sourceObject[key] = date.format(new Date(sourceObject[key]), 'yyyy-MM-dd hh:mm:ss')
+            var format = globalConfig.dateFormat
+            if (config.value  && config.value.length) {
+                format = config.value
+            }
+            if(source[key] === null){
+                source[key] = globalConfig.default
+                break
+            }
+            source[key] = date.format(new Date(source[key]), format)
+            break
+        case 'timestamp':
+            var formatDate = globalConfig.dateFormat
+            if (config.value  && config.value.length) {
+                formatDate = config.value
+            }
+            source[key] = date.parse(source[key], formatDate).getTime()
             break
         case 'enum':
-            var enumArray = config.value.split(',')
-            sourceObject[key] = enumArray[sourceObject[key]]
+            var enumArray = config.value
+            if (typeof config.value === 'string') {
+                enumArray = config.value.split(',')
+            }
+            source[key] = enumArray[source[key]]
+            break
+        case 'mapping':
+            var mapping = config.value
+            if (typeof config.value === 'string') {
+                mapping = {}
+                var mappingArray = config.value.split(',')
+                for (var i = 0; i < mappingArray.length; i++) {
+                    var map = mappingArray[i].split(':')
+                    mapping[map[0]] = map[1]
+                }
+            }
+            source[key] = mapping[source[key]]
             break
         case 'array':
             var splitChar = ','
-            if(config.value){
+            if (config.value) {
                 splitChar = config.value
             }
-            var array = sourceObject[key].split(splitChar)
-            sourceObject[key] = array
+            var array = source[key].split(splitChar)
+            source[key] = array
             break
         default:
-            globalExtend[config.option](sourceObject)
+            globalExtend[config.option](source, key, config.value)
             break
     }
 }
 
 /*
 * 转换单个数据对象
-* @param {Object} sourceObject - 操作数据对象
+* @param {Object} source - 操作数据对象
 * @param {Object} option - 参数
 * @return void
 * */
-function conversionObject(sourceObject, option) {
+function conversionObject(source, option) {
     for (var key in option) {
-        if(option[key] instanceof Array){
+        if (option[key] instanceof Array) {
             for (var i = 0; i < option[key].length; i++) {
-                singleConversion(sourceObject, option[key][i], key)
+                singleConversion(source, option[key][i], key)
             }
-        }else{
-            singleConversion(sourceObject, option[key], key)
+        } else {
+            singleConversion(source, option[key], key)
         }
     }
 }
@@ -106,23 +160,38 @@ function conversionObject(sourceObject, option) {
 * @param {Object, String} option - 配置参数
 * @return void
 * */
-function to(source, option, extend) {
-    if(extend) {
-        globalExtend = extend
-    }
-
+function to(source, option) {
     if (typeof option === 'string') {
         option = getOptionFromString(option)
     }
 
     if (source instanceof Array) {
-        // 如果是数组
         for (var i = 0; i < source.length; i++) {
             conversionObject(source[i], option)
         }
     } else {
         conversionObject(source, option)
     }
+}
+
+/*
+* 拓展方法
+* @param {Object} extend - 拓展对象（方法合集) { optionName: function(source, key, configValue) {}, ... }
+* @return to
+* */
+to.extend = function(extend) {
+    globalExtend = object.assign(globalExtend, extend)
+    return to
+}
+
+/*
+* 设置默认配置
+* @param {Object} config - 设置默认配置对象
+* @return to
+* */
+to.config = function(config) {
+    globalConfig = object.assign(globalConfig, config)
+    return to
 }
 
 module.exports = to
